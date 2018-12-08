7d7ef884d9d741a850767969e3bc7b4fe6f5a91d
zhengshaojian <zhengshaojian@LAPTOP-QOU8TM3B>
2018-12-08 15:20:19 +0800

    created by zhengsj

---
+++ src/main/java/com/example/zhengsj/ZhengsjApplication.java
@@ -0,0 +1,12 @@
+package com.example.zhengsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhengsjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhengsjApplication.class, args);
+	}
+}
